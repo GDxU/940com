0ab182aab12fc29fe5221ee2a4067db0ff7f7532
tjdreams <15221806781@163.com>
2016-07-01 09:02:40 +0800

    优化

--- node/blog/routes/index.js
+++ node/blog/routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'My Blog' });
 });
 
+var photos=[];
+photos.push()
 module.exports = router;
